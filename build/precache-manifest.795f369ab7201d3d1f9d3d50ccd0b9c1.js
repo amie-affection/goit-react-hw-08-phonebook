2add448c3db1109d04d978106b7afee4377dc750
amie-affection <amie.affection@gmail.com>
2020-07-27 17:15:33 +0300

    added netlify

---
+++ build/precache-manifest.795f369ab7201d3d1f9d3d50ccd0b9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd622d53ae23d2d3ad11047ccbaf8f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25d0da814ea68cbd7a9",
+    "url": "/static/css/main.1032cc12.chunk.css"
+  },
+  {
+    "revision": "99b5d979a36d2f4bfd79",
+    "url": "/static/js/2.07a8c1cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07a8c1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25d0da814ea68cbd7a9",
+    "url": "/static/js/main.fe2a3986.chunk.js"
+  },
+  {
+    "revision": "e082d9c2d50dc9442f8f",
+    "url": "/static/js/runtime-main.691d5af7.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-08-phonebook/precache-manifest.fcca16003417388ce3e28dc01b045bfc.js"
+  "/precache-manifest.795f369ab7201d3d1f9d3d50ccd0b9c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-08-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
